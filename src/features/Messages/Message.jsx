0089c9fdccd0bb0e68b59356a18d088563dc1a2a
Mike Parton <mikeparton@live.com.au>
2018-03-28 07:58:10 +1100

    Scroll down to see new messages automatically

--- src/features/Messages/Message.jsx
+++ src/features/Messages/Message.jsx
@@ -3,17 +3,25 @@ import withResource from 'shared/withResource'
 import { OuterMsgWrapper, MsgWrapper } from './styled'
 import { FlexDaddy } from 'shared/styled'
 
-const Message = ({ text, userId, currentUser }) => {
-  const isMine = userId == currentUser
-  return (
-    <OuterMsgWrapper>
-      <FlexDaddy justifyEnd={isMine}>
-        <MsgWrapper isMine={isMine}>
-          <div>{text}</div>
-        </MsgWrapper>
-      </FlexDaddy>
-    </OuterMsgWrapper>
-  )
+class Message extends React.Component {
+  componentDidMount() {
+    this.node && this.node.scrollIntoView()
+  }
+
+  render() {
+    const { text, userId, currentUser } = this.props
+    const isMine = userId == currentUser
+
+    return (
+      <OuterMsgWrapper innerRef={(ref) => { this.node = ref }}>
+        <FlexDaddy justifyEnd={isMine}>
+          <MsgWrapper isMine={isMine}>
+            <div>{text}</div>
+          </MsgWrapper>
+        </FlexDaddy>
+      </OuterMsgWrapper>
+    )
+  }
 }
 
 export default withResource('messages', Message)
